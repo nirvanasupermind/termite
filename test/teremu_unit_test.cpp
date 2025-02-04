038a50f6f6b08cb3fb23a33c58e4031a4c683dc0
nirvanasupermind <nirvana.supermind@gmail.com>
2025-02-03 22:21:04 -0800

    formatting

--- test/teremu_unit_test.cpp
+++ test/teremu_unit_test.cpp
@@ -15,7 +15,7 @@ int main(int argc, char** argv) {
     termite::CPU cpu;
     std::cout << "*** TEREMU UNIT TEST" << '\n';
     std::vector<std::vector<std::string> > tests {
-        {"push_pop", "DCD0 0000 0000 001A 0000 0000 DBC3 0000 0000 0000 0000 0000 DDCD D000 0000 0000 0000 00DC", "8"},
+        {"push_pop", "DCD0 0000 0000 001A 0000 0000 DBC3 0000 0000 0000 0000 0000 DDCD D000 0000 0000 0000 00DC", "7"},
         {"and_regs", "DDC3 D000 0000 0000 0000 001A DDC2 D000 0000 0000 0000 001B D1C3 C200 0000 0000 0000 0000 DDCD D000 0000 0000 0000 00DC", "5"},
         {"or_regs", "DDC3 D000 0000 0000 0000 001A DDC2 D000 0000 0000 0000 001B D2C3 C200 0000 0000 0000 0000 DDCD D000 0000 0000 0000 00DC", "10"},
         {"xor_regs", "DDC3 D000 0000 0000 0000 001A DDC2 D000 0000 0000 0000 001B D3C3 C200 0000 0000 0000 0000 DDCD D000 0000 0000 0000 00DC", "-8"}  
@@ -29,7 +29,7 @@ int main(int argc, char** argv) {
             std::cout << "test " + tests[i][0] + " passed";
             num_passed++;
         } else {
-            std::cout << "test " + tests[i][0] + " FAILED ";
+            std::cout << "test " + tests[i][0] + " FAILED";
         }
         std::cout << " with DX = " << cpu.regs[7].to_int32() << '\n';
         } catch(const std::string& e) {
