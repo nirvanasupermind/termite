515d7b99f4ba379e7efb819058efcc66035d365c
nirvanasupermind <nirvana.supermind@gmail.com>
2025-02-17 22:31:41 -0800

    anthill reset started.

---
+++ anthill/src/data.h
@@ -0,0 +1,64 @@
+#include "defs.h"
+
+#ifndef _extern
+#define _extern extern
+#endif
+
+_extern FILE *Infile;
+_extern FILE *Outfile;
+
+_extern int Token;
+_extern char Text[TEXTLEN+1];
+_extern int Value;
+_extern int Line;
+_extern int Errors;
+_extern int Syntoken;
+_extern int Putback;
+_extern int Rejected;
+_extern int Rejval;
+_extern char Rejtext[TEXTLEN+1];
+
+_extern char *File;
+_extern char *Basefile;
+
+_extern char *Macp[MAXNMAC];
+_extern int Macc[MAXNMAC];
+_extern int Mp;
+_extern int Expandmac;
+
+_extern int Ifdefstk[MAXIFDEF], Isp;
+_extern int Inclev;
+
+_extern int Textseg;
+
+_extern char *Names[NSYMBOLS];
+_extern char Prims [NSYMBOLS];
+_extern char Types [NSYMBOLS];
+_extern char Stcls [NSYMBOLS];
+_extern int Sizes [NSYMBOLS];
+_extern int Vals [NSYMBOLS];
+_extern char *Mtext[NSYMBOLS];
+
+_extern int Globs;
+_extern int Locs;
+
+_extern int Thisfn;
+
+_extern char Nlist[POOLSIZE];
+_extern int Nbot;
+_extern int Ntop;
+
+_extern char Nlist[POOLSIZE];
+_extern int Nbot;
+_extern int Ntop;
+
+
+
+_extern int Breakstk[MAXBREAK], Bsp;
+_extern int Contstk[MAXBREAK], Csp;
+_extern int Retlab;
+
+_extern int LIaddr[MAXLOCINIT];
+_extern int LIval[MAXLOCINIT];
+_extern int Nli;
+
--- anthill/src/func.cpp
+++
@@ -1,9 +0,0 @@
-#include <cinttypes>
-#include "static_type.h"
-#include "func.h"
-
-namespace anthill {
-    Func::Func(const StaticType& return_type, const std::vector<StaticType>& arg_types)
-        : return_type(return_type), arg_types(arg_types), var_counter(0)  {
-    }
-} // namespace anthill
