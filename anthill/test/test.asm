c6db0ba1c912a2359232fe7b35c28463f6f034bf
nirvanasupermind <nirvana.supermind@gmail.com>
2025-05-02 21:50:06 -0700

    print-str still not fixed

--- anthill/test/test.asm
+++ anthill/test/test.asm
@@ -59,28 +59,14 @@ pop %cx
 add %cx,%ax
 mov %ax,-2(%bp)
 _L2:
-mov -0(%bp),%ax
-mov -0(%bp),%ax
-push %ax
 mov -2(%bp),%ax
-pop %cx
-add %cx,%ax
-mov 0(%ax),%ax
-xor $0nDDDD, %ax
-mov -0(%bp),%ax
-mov -0(%bp),%ax
-push %ax
 mov -2(%bp),%ax
-pop %cx
-add %cx,%ax
-mov 0(%ax),%ax
-xor $0nDDDD, %ax
 push %ax
-mov $0,%ax
+mov $10,%ax
 pop %cx
 cmp %ax,%cx
 mov $1,%ax
-jne _L3
+jl _L3
 mov $-1,%ax
 _L3:
 cmp $1, %ax
@@ -94,15 +80,33 @@ int $0
 main:
 push %bp
 mov %sp,%bp
-mov $72,0
-mov $101,1
-mov $108,2
-push 4
-mov $0,3
-pop 4
 mov $0,%ax
+mov %ax,-0(%bp)
+jmp _L2
+_L1:
+mov -0(%bp),%ax
+mov -0(%bp),%ax
+push %ax
+mov $1,%ax
+pop %cx
+add %cx,%ax
+mov %ax,-0(%bp)
+_L2:
+mov -0(%bp),%ax
+mov -0(%bp),%ax
+push %ax
+mov $10,%ax
+pop %cx
+cmp %ax,%cx
+mov $1,%ax
+jl _L3
+mov $-1,%ax
+_L3:
+cmp $1, %ax
+je _L1
+mov -0(%bp),%ax
 mov %ax,%di
-call print_str
+call print_int
 mov $0,%ax
 pop %bp
 ret
