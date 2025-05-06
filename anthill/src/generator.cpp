c4f372e97ea061f933ad3da70b24b2d2ff6fa61c
nirvanasupermind <nirvana.supermind@gmail.com>
2025-05-05 22:03:31 -0700

    still not working after all of that

--- anthill/src/generator.cpp
+++ anthill/src/generator.cpp
@@ -41,17 +41,17 @@ namespace anthill {
    }
 
    std::string Generator::alloc_addr(const std::shared_ptr<NonFuncType>& type, const std::shared_ptr<SymbolTable>& symbol_table) {
-      // if (symbol_table->is_func) {
-      //    std::string result = "-" + std::to_string(func_addr_counter) + "(%bp)";
-      //    func_addr_counter += type->size();
-      //    std::cout << "dbg51 " << func_addr_counter << '\n';
-      //    return result;
-      // }
-      // else {
+      if (symbol_table->is_func) {
+         std::string result = "-" + std::to_string(func_addr_counter) + "(%bp)";
+         func_addr_counter += type->size();
+         std::cout << "dbg51 " << func_addr_counter << '\n';
+         return result;
+      }
+      else {
          std::string result = std::to_string(addr_counter);
          addr_counter += type->size();
          return result;
-      // }
+      }
    }
 
    void Generator::trunc_to_8_trits() {
@@ -713,7 +713,7 @@ namespace anthill {
       // asm_stream << "call main\nmov %ax,%dx\nmov $0nDD,%ax\nint $0\n";
       // }
       asm_stream << node->name.val << ":\n";
-      // asm_stream << "push %bp\nmov %sp,%bp\nsub %sp,729\n";
+      asm_stream << "push %bp\nmov %sp,%bp\nsub %sp,729\n";
       std::shared_ptr<SymbolTable> func_symbol_table = std::make_shared<SymbolTable>(SymbolTable(symbol_table));
       func_symbol_table->is_func = true;
       try {
@@ -746,14 +746,14 @@ namespace anthill {
       }
       // std::cout << "dbg430" << '\n';
       visit(node->body, func_symbol_table);
-      // asm_stream << "pop %bp\n";
+      asm_stream << "pop %bp\n";
       asm_stream << "ret\n";
       return std::make_shared<NonFuncType>(NonFuncType(BasicType::VOID));
    }
 
    std::shared_ptr<StaticType> Generator::visit_return_node(const std::shared_ptr<ReturnNode>& node, const std::shared_ptr<SymbolTable>& symbol_table) {
       visit(node->body, symbol_table);
-      // asm_stream << "pop %bp\n";
+      asm_stream << "pop %bp\n";
       asm_stream << "ret\n";
       return std::make_shared<NonFuncType>(NonFuncType(BasicType::VOID));
    }
--- anthill/test/test.asm
+++ anthill/test/test.asm
@@ -1,47 +1,60 @@
 putc:
+push %bp
+mov %sp,%bp
+sub %sp,729
 mov %di,%ax
-push 1
-mov %ax,0
-pop 1
-mov 0,%ax
+push 1(%bp)
+mov %ax,-0(%bp)
+pop 1(%bp)
+mov -0(%bp),%ax
 mov %ax,%dx
 mov $0nD0,%ax
 int $0
 mov %dx,%ax
+pop %bp
 ret
 puti:
+push %bp
+mov %sp,%bp
+sub %sp,729
 mov %di,%ax
-mov %ax,1
+mov %ax,-0(%bp)
 mov %di,%dx
 mov $0nDC,%ax
-jmp after_puti
+int $0
+mov %dx,%ax
+pop %bp
+ret
 puts:
+push %bp
+mov %sp,%bp
+sub %sp,729
 mov %di,%ax
-mov %ax,3
+mov %ax,-0(%bp)
 mov $0,%ax
-mov %ax,5
+mov %ax,-2(%bp)
 jmp _L2
 _L1:
-mov 3,%ax
-mov 3,%ax
+mov -0(%bp),%ax
+mov -0(%bp),%ax
 push %ax
-mov 5,%ax
+mov -2(%bp),%ax
 pop %cx
 add %cx,%ax
 mov 0(%ax),%ax
 xor $0nDDDD, %ax
 mov %ax,%di
 call putc
-mov 5,%ax
-mov 5,%ax
+mov -2(%bp),%ax
+mov -2(%bp),%ax
 push %ax
 mov $1,%ax
 pop %cx
 add %cx,%ax
-mov %ax,5
+mov %ax,-2(%bp)
 _L2:
-mov 5,%ax
-mov 5,%ax
+mov -2(%bp),%ax
+mov -2(%bp),%ax
 push %ax
 mov $10,%ax
 pop %cx
@@ -52,12 +65,17 @@ mov $-1,%ax
 _L3:
 cmp $1, %ax
 je _L1
+pop %bp
 ret
 main:
+push %bp
+mov %sp,%bp
+sub %sp,729
 mov $5,%ax
 mov %ax,%di
-jmp puti
-after_puti:
+call puti
 mov $0,%ax
+pop %bp
 ret
+pop %bp
 ret
