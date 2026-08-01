f805aeaa659589cf8dac66f23ce9ffbfbe0fa54b
nirvanasupermind <nirvana.supermind@gmail.com>
2026-08-01 02:27:37 -0700

    almost functional mul32

--- test/terfloat_manual_test.cpp
+++ test/terfloat_manual_test.cpp
@@ -6,14 +6,28 @@
 #include "../src/core/terfloat.h"
 
 int main() {
+    std::cerr << "entered main\n";
+
     termite::Word x = termite::Word::from_int32(10000);
     termite::Word y = termite::Word::from_int32(20000);
-    
 
+    std::cerr << "before mul32\n";
     auto result = x.mul32(y);
+    std::cerr << "after mul32\n";
 
     std::cout << result.first.to_int32() << '\n';
     std::cout << result.second.to_int32() << '\n';
-
-
 }
+
+// int main() {
+//     termite::Word x = termite::Word::from_int32(10000);
+//     termite::Word y = termite::Word::from_int32(20000);
+    
+
+//     auto result = x.mul32(y);
+
+//     std::cout << result.first.to_int32() << '\n';
+//     std::cout << result.second.to_int32() << '\n';
+
+
+// }
